bd73ae9c25dd410be2ce0669383c5d037437ac76
Daniel Bruna <ZOMBIEBUST3R@gmail.com>
2018-04-02 21:38:29 +0200

    Base para gestión de envios y ack

--- UDP_Taller6_Cliente/Source_Client.cpp
+++ UDP_Taller6_Cliente/Source_Client.cpp
@@ -5,22 +5,89 @@
 #define IP_SERVER "localhost"
 #define PORT_SERVER 50000
 
+enum Commands { HEY, CON, INF, NEW, MOV };
+
 int pos;
 sf::UdpSocket sock;
+int actualID = 0;
+
+struct Player
+{
+	sf::IpAddress ip;
+	unsigned short port;
+	sf::Vector2i pos;
+	int playerID;
+	sf::Color pjColor;
+};
+
+struct PendingMessage {
+	sf::Packet packet;
+	int id;
+	int time;
+};
+
+std::vector<Player> players = std::vector<Player>();
 
 //HACER UN SISTEMA PARA ENVIO HASTA CONFIRMACIÓN:
 	//UNA FUNCIÓN QUE SE LLAME EN EL RECEIVE QUE ELIMINE EL MENSAJE CONFIRMADO DE LA LISTA DE PENDIENTES. SE EJECUTA ANTES DEL ENVIO PARA NO ENVIAR ALGO YA CONFIRMADO.
 	//UNA FUNCIÓN QUE PASE POR UN VECTOR DE MESNAJES (ID, MSG, TIEMPO [se añade dt cada frame]) <int, packet, int(milisegundos)> Y LOS ENVIE CADA 500ms.
+std::vector<PendingMessage> messagePending = std::vector<PendingMessage>();
+
+void SendMessages(int deltaTime) {
+	//for()
+		//if(msg[i].time > 500ms) { reenviar + resetear a 0 el time }
+		//else { time + chronoDeltaTime.getTime().asMilliseconds(); }
+	for (auto i : messagePending) {
+		i.time += deltaTime;
+		if (i.time > 500) {
+			//RESEND MESSAGE
+			sock.send(i.packet, IP_SERVER, PORT_SERVER);
+			i.time = 0;
+		}
+	}
+	
+}
+
+void AddMessage(sf::Packet _pack) {	//SOLO PARA EL HEY
+	PendingMessage tempPending;
+	tempPending.id = actualID;
+	_pack << actualID;
+	tempPending.packet = _pack;
+	tempPending.time = 0;
+
+	sock.send(_pack, IP_SERVER, PORT_SERVER);
+	messagePending.push_back(tempPending);
+
+	actualID++;
+}
+
+void MessageConfirmed(int _ID) {	// SOLO PARA EL CON
+	int indexToDelete = 0;
+	bool found = false;
+	std::vector<PendingMessage>::iterator iteratorToDelete = messagePending.begin();
+
+	while( !found || (indexToDelete < messagePending.size()) ) {	//if(iteratorToDelete != messagePending.end()) ALTERNATIVA PARA NO USAR EL INDEXTODELETE
+		if (iteratorToDelete->id == _ID) {
+			//FOUND MY MESSAGE TO DELETE
+			found = true;
+		}
+		else { indexToDelete++; iteratorToDelete++; }
+	}
+
+	if (found) { messagePending.erase(iteratorToDelete); }
+}
+
 
 void DibujaSFML()
 {
 	sf::RenderWindow window(sf::VideoMode(800, 600), "Sin acumulación en cliente");
+	
+	//INICIAR CHRONO PARA DELTATIME ----------------------------------------------
+	sf::Clock chronoDeltaTime;
 
 	while (window.isOpen())
 	{
 		sf::Event event;
-
-		//INICIAR CHRONO PARA DELTATIME ----------------------------------------------
 
 		while (window.pollEvent(event))
 		{
@@ -36,7 +103,7 @@ void DibujaSFML()
 					int posAux = pos - 1;
 					pckLeft << posAux;
 					sock.send(pckLeft, IP_SERVER, PORT_SERVER);
-					//AÑADIR MENSAJE A LISTA DE PENDIENTES: ID un int que aumenta a cada mensaje añadido | msg es el packete | time iniciado a 0 (milisegundos)
+					//AÑADIR MENSAJE A LISTA DE PENDIENTES: ID un int que aumenta a cada mensaje añadido | msg es el packete | time iniciado a 0 (milisegundos) MOV
 
 				}
 				else if (event.key.code == sf::Keyboard::Right)
@@ -45,7 +112,7 @@ void DibujaSFML()
 					int posAux = pos + 1;
 					pckRight << posAux;
 					sock.send(pckRight, IP_SERVER, PORT_SERVER);
-					//AÑADIR MENSAJE A LISTA DE PENDIENTES: ID un int que aumenta a cada mensaje añadido | msg es el packete | time iniciado a 0 (milisegundos)
+					//AÑADIR MENSAJE A LISTA DE PENDIENTES: ID un int que aumenta a cada mensaje añadido | msg es el packete | time iniciado a 0 (milisegundos) MOV
 				}
 				break;
 				
@@ -68,11 +135,8 @@ void DibujaSFML()
 		}
 
 		//REENVIAR PENDIENTES CADA 500ms.
-			//for()
-				//if(msg[i].time > 500ms) { reenviar + resetear a 0 el time }
-				//else { time + chronoDeltaTime.getTime().asMilliseconds(); }
-			
-			//chronoDeltaTime.reset();
+		SendMessages(chronoDeltaTime.getElapsedTime().asMilliseconds());
+		chronoDeltaTime.restart();
 
 		window.clear();
 
--- UPD_Taller6/Source_Server.cpp
+++ UPD_Taller6/Source_Server.cpp
@@ -5,6 +5,11 @@
 #define RIGHT_POS 600
 #define LEFT_POS 200
 
+enum Commands { HEY, CON, INF, NEW, MOV };
+
+int actualID = 0;
+sf::UdpSocket sock;
+
 struct Player
 {
 	sf::IpAddress ip;
@@ -14,11 +19,64 @@ struct Player
 	sf::Color pjColor;
 };
 
+struct PendingMessage {
+	sf::Packet packet;
+	int id;
+	int time;
+	sf::IpAddress targetAdress;
+	unsigned short port;
+};
+
+std::vector<PendingMessage> messagePending = std::vector<PendingMessage>();
+
+void AddMessage(sf::Packet _pack, sf::IpAddress _ip, unsigned short _port) {	// SOLO PARA EL NEW
+	PendingMessage tempPending;
+	tempPending.id = actualID;
+	_pack << actualID;
+	tempPending.packet = _pack;
+	tempPending.targetAdress = _ip;
+	tempPending.port = _port;
+	tempPending.time = 0;
+
+	sock.send(_pack, _ip, _port);
+	messagePending.push_back(tempPending);
+
+	actualID++;
+}
+
+void SendMessages(int deltaTime) {
+	for (auto i : messagePending) {
+		i.time += deltaTime;
+		if (i.time > 500) {
+			//RESEND MESSAGE
+			sock.send(i.packet, i.targetAdress, i.port);
+			i.time = 0;
+		}
+	}
+
+}
+
+void MessageConfirmed(int _ID) {	//SOLO PARA EL ACK
+	int indexToDelete = 0;
+	bool found = false;
+	std::vector<PendingMessage>::iterator iteratorToDelete = messagePending.begin();
+
+	while (!found || (indexToDelete < messagePending.size())) {	//if(iteratorToDelete != messagePending.end()) ALTERNATIVA PARA NO USAR EL INDEXTODELETE
+		if (iteratorToDelete->id == _ID) {
+			//FOUND MY MESSAGE TO DELETE
+			found = true;
+		}
+		else { indexToDelete++; iteratorToDelete++; }
+	}
+
+	if (found) { messagePending.erase(iteratorToDelete); }
+}
+
 int main()
 {
 	std::vector<Player> players = std::vector<Player>();
 	
-	sf::UdpSocket sock;
+	
 	sf::Socket::Status status = sock.bind(50000);
 	if (status != sf::Socket::Done)
 	{
@@ -27,6 +85,7 @@ int main()
 		exit(0);
 	}
 	sf::Clock clock;
+	sf::Clock clockDeltaTime;
 	do
 	{
 		if (clock.getElapsedTime().asMilliseconds() > 200)
@@ -50,11 +109,15 @@ int main()
 					sf::Packet pckSend;
 					pckSend << pos;
 					sock.send(pckSend, ipRem, portRem);
-
+					
 				}
 			}
 			clock.restart();
 		}
+
+		SendMessages(clockDeltaTime.getElapsedTime().asMilliseconds());
+		clockDeltaTime.restart();
+
 	} while (true);
 	sock.unbind();
 	return 0;
